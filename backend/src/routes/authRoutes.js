05966430c097f97b98cfab8614f7bb59698ebd96
Gorantla Mokshgna <gorantlamokshgna980@gmail.com>
2025-06-16 08:43:36 +0530

    added routes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,5 +1,15 @@
 import express from "express";
+import { login, logout, logoutAllSessions, resetPassword, sendotp, signUp, verifyEmail } from "../controllers/authController";
+import { protectRoute } from "../middleWare/authMiddleWare.js";
+
 const router = express.Router();
 
-router.post("/signIn",signIn);
+router.post("/signIn",signUp);
 router.post("/login",login);
+router.post("/verifyEmail",protectRoute,verifyEmail);
+router.post("/resetPassword",resetPassword);
+router.post("/logoutAllSessions",protectRoute,logoutAllSessions);
+router.post("/logout",protectRoute,logout);
+router.post("/sendOtp",sendotp);
+
+export default router;
